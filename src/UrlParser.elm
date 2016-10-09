bb27b477587260c23d26d862844a4196026e3a8d
Evan Czaplicki <evancz@users.noreply.github.com>
2016-10-09 15:00:58 -0700

    Add doc comments for all Parser values

--- src/UrlParser.elm
+++ src/UrlParser.elm
@@ -49,16 +49,35 @@ type alias State value =
 -- PARSE SEGMENTS
 
 
+{-| Parse a segment of the path as a `String`.
+
+    parsePath string location
+    -- /alice/  ==>  Just "alice"
+    -- /bob     ==>  Just "bob"
+    -- /42/     ==>  Just "42"
+-}
 string : Parser (String -> a) a
 string =
   custom "STRING" Ok
 
 
+{-| Parse a segment of the path as an `Int`.
+
+    parsePath int location
+    -- /alice/  ==>  Nothing
+    -- /bob     ==>  Nothing
+    -- /42/     ==>  Just 42
+-}
 int : Parser (Int -> a) a
 int =
   custom "NUMBER" String.toInt
 
 
+{-| Parse a segment of the path if it matches a given string.
+
+    s "blog"  -- can parse /blog/
+              -- but not /glob/ or /42/ or anything else
+-}
 s : String -> Parser a a
 s str =
   Parser <| \{ visited, unvisited, params, value } ->
@@ -74,6 +93,25 @@ s str =
           []
 
 
+{-| Create a custom path segment parser. Here is how it is used to define the
+`int` and `string` parsers:
+
+    int =
+      custom "NUMBER" String.toInt
+
+    string =
+      custom "STRING" Ok
+
+You can use it to define something like “only CSS files” like this:
+
+    css : Parser (String -> a) a
+    css =
+      custom "CSS_FILE" <| \segment ->
+        if String.endsWith ".css" then
+          Ok segment
+        else
+          Err "Does not end with .css"
+-}
 custom : String -> (String -> Result String a) -> Parser (a -> b) b
 custom tipe stringToSomething =
   Parser <| \{ visited, unvisited, params, value } ->
@@ -94,15 +132,46 @@ custom tipe stringToSomething =
 -- COMBINING PARSERS
 
 
-infixr 7 </>
+{-| Parse a path with multiple segments.
 
+    parsePath (s "blog" </> int) location
+    -- /blog/35/  ==>  Just 35
+    -- /blog/42   ==>  Just 42
+    -- /blog/     ==>  Nothing
+    -- /42/       ==>  Nothing
 
+    parsePath (s "search" </> string) location
+    -- /search/cats/  ==>  Just "cats"
+    -- /search/frog   ==>  Just "frog"
+    -- /search/       ==>  Nothing
+    -- /cats/         ==>  Nothing
+-}
 (</>) : Parser a b -> Parser b c -> Parser a c
 (</>) (Parser parseBefore) (Parser parseAfter) =
   Parser <| \state ->
     List.concatMap parseAfter (parseBefore state)
 
 
+infixr 7 </>
+
+
+{-| Transform a path parser.
+
+    type alias Comment = { author : String, id : Int }
+
+    rawComment : Parser (String -> Int -> a) a
+    rawComment =
+      s "user" </> string </> s "comments" </> int
+
+    comment : Parser (Comment -> a) a
+    comment =
+      map Comment rawComment
+
+    parsePath comment location
+    -- /user/bob/comments/42  ==>  Just { author = "bob", id = 42 }
+    -- /user/tom/comments/35  ==>  Just { author = "tom", id = 35 }
+    -- /user/sam/             ==>  Nothing
+-}
 map : a -> Parser a b -> Parser (b -> c) c
 map subValue (Parser parse) =
   Parser <| \{ visited, unvisited, params, value } ->
@@ -123,12 +192,57 @@ mapHelp func {visited, unvisited, params, value} =
   }
 
 
+{-| Try a bunch of different path parsers.
+
+    type Route
+      = Search String
+      | Blog Int
+      | User String
+      | Comment String Int
+
+    route : Parser (Route -> a) a
+    route =
+      oneOf
+        [ map Search  (s "search" </> string)
+        , map Blog    (s "blog" </> int)
+        , map User    (s "user" </> string)
+        , map Comment (s "user" </> string </> "comments" </> int)
+        ]
+
+    parsePath route location
+    -- /search/cats           ==>  Just (Search "cats")
+    -- /search/               ==>  Nothing
+
+    -- /blog/42               ==>  Just (Blog 42)
+    -- /blog/cats             ==>  Nothing
+
+    -- /user/sam/             ==>  Just (User "sam")
+    -- /user/bob/comments/42  ==>  Just (Comment "bob" 42)
+    -- /user/tom/comments/35  ==>  Just (Comment "tom" 35)
+    -- /user/                 ==>  Nothing
+
+-}
 oneOf : List (Parser a b) -> Parser a b
 oneOf parsers =
   Parser <| \state ->
     List.concatMap (\(Parser parser) -> parser state) parsers
 
 
+{-| A parser that does not consume any path segments.
+
+    type BlogRoute = Overview | Post Int
+
+    blogRoute : Parser (BlogRoute -> a) a
+    blogRoute =
+      oneOf
+        [ map Overview top
+        , map Post  (s "post" </> int)
+        ]
+
+    parsePath (s "blog" </> blogRoute) location
+    -- /blog/                 ==>  Just Overview
+    -- /blog/post/42          ==>  Just (Post 42)
+-}
 top : Parser a a
 top =
   Parser <| \state -> [state]
